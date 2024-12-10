9e286595e7937abb232fdff7287757ac15c82bd0
Mateo071 <floresmateo2000@gmail.com>
2024-12-10 16:19:20 -0500

    removed comments

--- main.py
+++ main.py
@@ -23,31 +23,24 @@ screen.onkey(snake.down, "s")
 screen.onkey(snake.right, "d")
 
 while is_game_on:
-#   for square in snake_squares:
-
 
   screen.update()
   time.sleep(0.1)
   snake.move()
 
-  #Detect collision with food 
   if snake.head.distance(food) < 15:
     food.refresh()
     score.update_score(1)
     snake.grow()
 
-  #Detect collision with wall
   if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290 :
     is_game_on = False
     score.game_over()
     print(f"GG. Your final score was {score.is_score()}. 🏆")
 
-  #Detect collision with tail
   for segment in snake.segments[1:]:
     if snake.head.distance(segment) < 10:
       is_game_on = False
       score.game_over()
-  #If head collides with any segment in the tail:
-    #trigger game_over
 
 screen.exitonclick()
